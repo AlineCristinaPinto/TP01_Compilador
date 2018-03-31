b7db3a602440b260dac23ba4a629e653fbbd99b5
Dlinha <ninanerd15@gmail.com>
2018-03-31 11:31:16 -0300

    Variavel e estrutura de Memoria

--- CompiladorCefetiny/src/compiladorcefetiny/Memoria.java
+++ CompiladorCefetiny/src/compiladorcefetiny/Memoria.java
@@ -1,8 +1,23 @@
 package compiladorcefetiny;
+
+import java.util.HashSet;
+
 /**
  *
  * @author Aline, Eduardo Cotta, Luiz, Pedro Lucas e Ruan
  */
 public class Memoria {
+    private HashSet<Variavel> pilhaMem;
     
+    public Boolean Search (){
+        return false;
+    }
+    
+    public void Insert (){
+    
+    }
+    
+    public void Remove (){
+    
+    }
 }
